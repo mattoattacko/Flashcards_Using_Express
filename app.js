083e26d6d9a04336177d0eeb3484e22d0b894249
Matto <matt.mcquain@gmail.com>
2019-10-14 21:23:50 -0700

    added basic routing

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const app = express();
 
+// Route Route
+// we use an anon callback function that will run when the client requests this route
+app.get('/', (request, response)  => {
+  response.send('Aloha');
+});
+
 app.listen(3000);
