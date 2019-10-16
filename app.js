0aea7de05eb692c33ed9cd69861b2e70f0270d6e
Matto <matt.mcquain@gmail.com>
2019-10-15 20:41:20 -0700

    added Pug and new routing

--- app.js
+++ app.js
@@ -2,10 +2,30 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'pug');
+
 // Route Route
 // we use an anon callback function that will run when the client requests this route
-app.get('/', (request, response)  => {
-  response.send('Aloha');
+// this block is functionally the same as the one below, just written out in long format.
+// app.get('/', (request, response)  => {
+//   response.send('Aloha');
+// });
+
+// app.get('/', (req, res) => {
+//   res.send('<h1>Aloha 1</h1>');
+// });
+/**
+* Once we set up Pug, we switch res.send() to res.render() in our index route. We use the name of the pug template as the parameter
+*/
+
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-app.listen(3000);
+app.get('/hello', (req, res) => {
+  res.send('<h2>Aloha 2</h2>');
+});
+
+app.listen(3000, () => {
+  console.log('The application is running on localhost:3000')
+});
