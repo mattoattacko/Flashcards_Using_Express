44ad6232c5f581965cd5d5196436a3706dcc41c4
Matto <matt.mcquain@gmail.com>
2019-10-22 20:57:14 -0700

    added goodbye button to earase usernames

--- app.js
+++ app.js
@@ -23,10 +23,16 @@ app.set('view engine', 'pug');
 * Once we set up Pug, we switch res.send() to res.render() in our index route. We use the name of the pug template as the parameter
 */
 
+// This is the index route
 app.get('/', (req, res) => {
   const name = req.cookies.username;
   // Here, since both the key and the value have the same name, we can use the ES6 shorthand to remove the colon and extra name. So instead of {name:name}, we can do just {name}.
-  res.render('index', { name });
+  // If the value of "name" exists, we want to render the index template. Else, redirect to the hellow route. 
+  if (name) {
+    res.render('index', { name });
+  } else {
+    res.redirect('/hello');
+  }
 });
 
 app.get('/cards', (req, res) => {
@@ -36,7 +42,13 @@ app.get('/cards', (req, res) => {
 
 // Here is a simple example of adding a route and rendering it. This get route is for serving the form itself.
 app.get('/hello', (req, res) => {
-  res.render('hello');
+  // If the value of "name" exists already, we want to redirect them to the index route, else render the "hello" form. Basically this redirects based on the cookies value. 
+  const name = req.cookies.username;
+  if (name) {
+    res.redirect('/');
+  } else {
+    res.render('hello');
+  }
 });
 
 // This rerenders the form after we send the name to the server.
@@ -45,6 +57,12 @@ app.post('/hello', (req, res) => {
   res.cookie('username', req.body.username);
   // Here we are passing in the name our user enters to the render method. 
   res.redirect('/'); 
+});
+
+// This POST route uses the clearCookie method to clear the cookies unsername value. It then redirects people to the "hello" route. 
+app.post('/goodbye', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/hello');
 });
 
 app.listen(3000, () => {
