689130c2fcf93308fb6676ed9dc673852c3b784a
Matto <matt.mcquain@gmail.com>
2019-10-28 21:23:39 -0700

    added a flat data file for testing purposes

--- routes/cards.js
+++ routes/cards.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const { data } = require('./data/flashcardData.json');
+const { cards } = data;
 
-
-router.get('/', (req, res) => {
+router.get('/:id', (req, res) => {
   // here we pass in an object with the property "prompt", set to a question we want to ask. 
-  res.render('card', { prompt: "Who is buried in Grants Tomb?" });
+  // res.render('card', { prompt: "Who is buried in Grants Tomb?" });
+
+  // We change our code from above to this. We connect the template to the first question. In the obeject we are passing in to the template, we point the "prompt" property to the first cards questions property. Also hint to the cards hint. 
+  res.render('card', {
+    prompt: cards[req.params.id].question,
+    hint: cards[req.params.id].hint
+  });
 });
 
 module.exports = router;
