f8f08d7166867d159e1a0da5957247252af42c28
Matthew McQuain <matt.mcquain@gmail.com>
2019-10-29 19:32:11 -0700

    refining the card template

--- routes/cards.js
+++ routes/cards.js
@@ -4,14 +4,30 @@ const { data } = require('./data/flashcardData.json');
 const { cards } = data;
 
 router.get('/:id', (req, res) => {
+  // Here we check for the presence of a query string key side. Assigned to var "side".
+  const { side } = req.query;
+  // Since "question" and "answer" are properties on the json, we can use the value stored in "side" to find the text we want to display.
+  // We need the id of the card. We create a variable to hold the id from the route paramater. 
+  const { id } = req.params;
+  // Now we can access the two pieces of text that we want to use. We store it in a constant named "text".
+  const text = cards[id][side];
+  // We also want to store a reference to the hint. 
+  const { hint } = cards[id];
+
+  // Now we route the text and the hint into an object that we can pass into the template. 
+  const templateData = { text, hint};
+  res.render('card', templateData)
+});
+  // Lastly, we need to change the variable name in the card template from "card" to "text". This will ensure the question or answer will show up. 
+
   // here we pass in an object with the property "prompt", set to a question we want to ask. 
   // res.render('card', { prompt: "Who is buried in Grants Tomb?" });
 
   // We change our code from above to this. We connect the template to the first question. In the obeject we are passing in to the template, we point the "prompt" property to the first cards questions property. Also hint to the cards hint. 
-  res.render('card', {
-    prompt: cards[req.params.id].question,
-    hint: cards[req.params.id].hint
-  });
-});
+  // res.render('card', {
+  //   prompt: cards[req.params.id].question,
+  //   hint: cards[req.params.id].hint
+  // });
+
 
 module.exports = router;
